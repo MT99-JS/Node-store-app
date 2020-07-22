4090529fdd51915ba0bfe59aeaf86b49753048df
Muhammad Yasir <taimoormuhammad009@gmail.com>
2020-07-22 17:08:13 +0500

    Added validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const bcrypt = require("bcryptjs");
 const nodemailer = require("nodemailer");
 const sendgridtransport = require("nodemailer-sendgrid-transport");
+const { validationResult } = require("express-validator");
 
 const User = require("../models/user");
 
@@ -76,6 +77,15 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(errors.array());
+    return res.status(422).render("auth/signup", {
+      path: "/signup",
+      pageTitle: "Signup",
+      errorMessage: errors.array()[0].msg,
+    });
+  }
   User.findOne({ email: email })
     .then((userDoc) => {
       if (userDoc) {
--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,35 @@
-const express = require('express');
+const express = require("express");
 
-const authController = require('../controllers/auth');
+const { check, body } = require("express-validator");
+
+const authController = require("../controllers/auth");
 
 const router = express.Router();
 
-router.get('/login', authController.getLogin);
+router.get("/login", authController.getLogin);
 
-router.get('/signup', authController.getSignup);
+router.get("/signup", authController.getSignup);
 
-router.post('/login', authController.postLogin);
+router.post("/login", authController.postLogin);
 
-router.post('/signup', authController.postSignup);
+router.post(
+  "/signup",
+  [
+    check("email").isEmail().withMessage("Please enter a valid email"),
+    body(
+      "password",
+      "Password should contain atleast 5 characters and consist of both numbers and alphabets "
+    ).isLength({ min: 5 }),
+    body("confirmPassword").custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("Password have to match");
+      }
+      return true;
+    }),
+  ],
+  authController.postSignup
+);
 
-router.post('/logout', authController.postLogout);
+router.post("/logout", authController.postLogout);
 
 module.exports = router;
